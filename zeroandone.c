0d92c4c44a211fb71ebf6bfb8482908b2adc4fc9
TeamExotic69 <149905379+TeamExotic69@users.noreply.github.com>
2024-01-09 13:53:55 +0530

    Add files via upload

---
+++ zeroandone.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    
+    for(int i=1; i<=4; i++){
+       // if(i%2!=0) a= 1;
+      //  else a=0;
+        for(int j=1; j<=i; j++){
+          //printf("%d",a);
+          //if(a=0)a=1;
+          //else a=o;
+         if((i+j)%2==0 ) print("1 ");
+         else print("0 ");
+        }
+        printf("\n");
+    }
+}
